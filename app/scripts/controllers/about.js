128a4f48648b76f36bbf960cceae6c5290f7adf8
ashtindell <tindell.ea@gmail.com>
2014-12-12 10:41:56 -0500

    first commit and yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name craveAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the craveAngularApp
+ */
+angular.module('craveAngularApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
